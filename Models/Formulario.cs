8690a1ad229ea45a3d62e4447801b612a33b1224
Your NameRicardo <ricardin.germanoo@gmail.com>
2021-06-12 01:35:10 -0300

    Projeto Finalizado

---
+++ Models/Formulario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESTE_ENTREVISTA.Models
+{
+    public class Formulario
+    {
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        public string mensagem { get; set; }
+        public string emailComTelefone { get; set; }
+    }
+}
